70ed9ab5e6b7f7d3a8a7d8dbc546609b5754428a
Adroaldo <hygino@msn.com>
2021-06-18 07:40:14 -0300

    Adicionado barra de scroll e botões extras funcionais

--- src/Teste.java
+++ src/Teste.java
@@ -1,23 +1,24 @@
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 import entities.Product;
-import javax.swing.SwingConstants;
-import javax.swing.DefaultComboBoxModel;
-import java.awt.Font;
+import java.awt.Color;
 
 public class Teste {
 
@@ -65,7 +66,7 @@ public class Teste {
 	private void initialize() {
 		frmGeradorDeCustos = new JFrame();
 		frmGeradorDeCustos.setTitle("Gerador de custos");
-		frmGeradorDeCustos.setBounds(100, 100, 598, 522);
+		frmGeradorDeCustos.setBounds(100, 100, 613, 522);
 		frmGeradorDeCustos.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmGeradorDeCustos.getContentPane().setLayout(null);
 
@@ -74,8 +75,9 @@ public class Teste {
 		frmGeradorDeCustos.getContentPane().add(lblNewLabel);
 
 		txtNome = new JTextField();
+		txtNome.setText("banha");
 		txtNome.setFont(new Font("SansSerif", Font.PLAIN, 14));
-		txtNome.setBounds(10, 48, 86, 20);
+		txtNome.setBounds(10, 48, 86, 32);
 		frmGeradorDeCustos.getContentPane().add(txtNome);
 		txtNome.setColumns(10);
 
@@ -93,7 +95,7 @@ public class Teste {
 		JComboBox<String> cbTipo = new JComboBox<String>();
 		cbTipo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (cbTipo.getSelectedItem().equals("SÓLIDO")) {
+				if (cbTipo.getSelectedItem().equals("SOLIDO")) {
 					cbUnidadeComprada.setModel(new DefaultComboBoxModel<String>(new String[] { "Kg", "g" }));
 					cbUnidadeUsada.setModel(new DefaultComboBoxModel<String>(new String[] { "Kg", "g" }));
 				} else {
@@ -103,9 +105,9 @@ public class Teste {
 
 			}
 		});
-		cbTipo.setModel(new DefaultComboBoxModel<String>(new String[] { "S\u00D3LIDO", "L\u00CDQUIDO" }));
+		cbTipo.setModel(new DefaultComboBoxModel<String>(new String[] { "SOLIDO", "LIQUIDO" }));
 
-		cbTipo.setBounds(106, 47, 91, 22);
+		cbTipo.setBounds(106, 47, 91, 33);
 		frmGeradorDeCustos.getContentPane().add(cbTipo);
 
 		JLabel lblNewLabel_1 = new JLabel("Tipo");
@@ -113,8 +115,9 @@ public class Teste {
 		frmGeradorDeCustos.getContentPane().add(lblNewLabel_1);
 
 		txtQuantidadeComprada = new JTextField();
+		txtQuantidadeComprada.setText("3");
 		txtQuantidadeComprada.setFont(new Font("SansSerif", Font.PLAIN, 14));
-		txtQuantidadeComprada.setBounds(209, 48, 78, 20);
+		txtQuantidadeComprada.setBounds(209, 48, 78, 32);
 		frmGeradorDeCustos.getContentPane().add(txtQuantidadeComprada);
 		txtQuantidadeComprada.setColumns(10);
 
@@ -123,33 +126,42 @@ public class Teste {
 		frmGeradorDeCustos.getContentPane().add(lblNewLabel_2);
 
 		txtValorComprado = new JTextField();
+		txtValorComprado.setText("45");
 		txtValorComprado.setFont(new Font("SansSerif", Font.PLAIN, 14));
-		txtValorComprado.setBounds(299, 48, 86, 20);
+		txtValorComprado.setBounds(299, 48, 86, 32);
 		frmGeradorDeCustos.getContentPane().add(txtValorComprado);
 		txtValorComprado.setColumns(10);
 
 		txtQuantidadeUsada = new JTextField();
+		txtQuantidadeUsada.setText("0,5");
 		txtQuantidadeUsada.setFont(new Font("SansSerif", Font.PLAIN, 14));
 		txtQuantidadeUsada.setColumns(10);
-		txtQuantidadeUsada.setBounds(397, 48, 78, 20);
+		txtQuantidadeUsada.setBounds(397, 48, 78, 32);
 		frmGeradorDeCustos.getContentPane().add(txtQuantidadeUsada);
 
 		JLabel lbContador = new JLabel("0");
 		lbContador.setHorizontalAlignment(SwingConstants.CENTER);
-		lbContador.setBounds(497, 130, 72, 16);
+		lbContador.setBounds(497, 178, 72, 16);
 		frmGeradorDeCustos.getContentPane().add(lbContador);
 
 		JLabel lbCustoTotal = new JLabel("R$ 0,00");
 		lbCustoTotal.setHorizontalAlignment(SwingConstants.CENTER);
-		lbCustoTotal.setBounds(497, 186, 75, 16);
+		lbCustoTotal.setBounds(497, 226, 75, 16);
 		frmGeradorDeCustos.getContentPane().add(lbCustoTotal);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 92, 465, 365);
+		frmGeradorDeCustos.getContentPane().add(scrollPane);
+
+		JTextArea txtLista = new JTextArea();
+		scrollPane.setViewportView(txtLista);
 
 		JButton btAdiciona = new JButton("Adiciona");
 		btAdiciona.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 
 				try {
-					String nome = txtNome.getText().toUpperCase();//deixa o nome em maiúscula
+					String nome = txtNome.getText().toUpperCase();// deixa o nome em maiúscula
 					String tipoUnidade = cbTipo.getSelectedItem().toString();
 					String tipoComprado = cbUnidadeComprada.getSelectedItem().toString();
 					double quantidadeComprada = Double.parseDouble(txtQuantidadeComprada.getText().replace(",", "."));
@@ -172,13 +184,8 @@ public class Teste {
 				}
 			}
 		});
-		btAdiciona.setBounds(487, 29, 89, 23);
+		btAdiciona.setBounds(487, 30, 89, 23);
 		frmGeradorDeCustos.getContentPane().add(btAdiciona);
-
-		JTextArea txtLista = new JTextArea();
-		txtLista.setFont(new Font("SansSerif", Font.PLAIN, 16));
-		txtLista.setBounds(10, 108, 465, 358);
-		frmGeradorDeCustos.getContentPane().add(txtLista);
 
 		JButton btLimpa = new JButton("Limpa");
 		btLimpa.addActionListener(new ActionListener() {
@@ -189,7 +196,7 @@ public class Teste {
 				txtQuantidadeUsada.setText(null);
 			}
 		});
-		btLimpa.setBounds(487, 57, 89, 23);
+		btLimpa.setBounds(487, 54, 89, 23);
 		frmGeradorDeCustos.getContentPane().add(btLimpa);
 
 		JButton btLista = new JButton("Listar");
@@ -197,28 +204,58 @@ public class Teste {
 			public void actionPerformed(ActionEvent e) {
 				if (lista.size() > 0) {
 					double somaTotal = 0.0;
-
+					int cont = 0;
 					for (Product p : lista) {
 						somaTotal += p.geraCusto();
+						cont++;
+						txtLista.append("Ingrediente #" + cont + " ");
 						txtLista.append(p.toString());
 						txtLista.append("------------------------------------------\n");
 					}
 					txtLista.append("O custo total foi de R$" + String.format("%.2f", somaTotal));
+
 				}
 			}
 		});
-		btLista.setBounds(487, 81, 89, 23);
+		btLista.setBounds(487, 78, 89, 23);
 		frmGeradorDeCustos.getContentPane().add(btLista);
 
 		JLabel lblNewLabel_3 = new JLabel("Ingredientes");
 		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_3.setBounds(487, 114, 89, 16);
+		lblNewLabel_3.setBounds(487, 150, 89, 16);
 		frmGeradorDeCustos.getContentPane().add(lblNewLabel_3);
 
 		JLabel lblNewLabel_4 = new JLabel("Custo");
 		lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_4.setBounds(507, 158, 55, 16);
+		lblNewLabel_4.setBounds(507, 198, 55, 16);
 		frmGeradorDeCustos.getContentPane().add(lblNewLabel_4);
+
+		JButton btCopia = new JButton("Copiar");
+		btCopia.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				txtLista.selectAll();
+				txtLista.copy();
+			}
+		});
+		btCopia.setBounds(487, 102, 89, 23);
+		frmGeradorDeCustos.getContentPane().add(btCopia);
+
+		JButton btnova = new JButton("Nova");
+		btnova.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				contador = 0;
+				custoTotal = 0.0;
+				txtLista.setText(null);
+				txtNome.setText(null);
+				txtQuantidadeComprada.setText(null);
+				txtValorComprado.setText(null);
+				txtQuantidadeUsada.setText(null);
+
+			}
+		});
+		btnova.setBackground(Color.GREEN);
+		btnova.setBounds(487, 6, 89, 23);
+		frmGeradorDeCustos.getContentPane().add(btnova);
 
 	}
 
